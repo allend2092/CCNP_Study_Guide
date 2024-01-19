2da6bf20e8a53906d3ece282a076048bbf00890b
Daryl <42759316+allend2092@users.noreply.github.com>
2024-01-18 21:39:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -20,4 +20,5 @@ def index():
     return render_template('index.html', card=flashcards[card_index], topic=topic, card_index=card_index, total_cards=len(flashcards), topics=topics)
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000)
+
